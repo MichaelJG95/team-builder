b894ba9df4734ece3f5baf393f4e093523c2b574
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-02-22 15:18:32 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team Builder</h1>
+      <form>
+        <input 
+          name="teamMemberName"
+          type="text"
+        />
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
