287193a69a9e88b4bd08a2a8e692de1d1646fb78
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-02-22 15:50:34 -0600

    commit for display team member list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import React, { useState } from 'react';
+
+
+const TeamMembersList = [
+  { name: 'Mike', email: 'mike@me.com', role: 'student', id: 0 },
+  { name: 'Mark', email: 'theZuck@meta.com', role: 'reptilian', id: 1 },
+  { name: 'Joe', email: 'potus@whitehouse.gov', role: 'president', id: 2 },
+]
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState(TeamMembersList)
+
   return (
     <div className="App">
       <h1>Team Builder</h1>
@@ -11,6 +21,7 @@ function App() {
         />
 
       </form>
+      {teamMembers.map(member => <h2 key={member.id}>{member.name} email: {member.email} role: {member.role} </h2>)}
     </div>
   );
 }
